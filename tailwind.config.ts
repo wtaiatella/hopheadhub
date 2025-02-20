c2bb9b67f2b2c958605b2a014e79f77c939f2836
Wagner Taiatella <wtaiatella@gmail.com>
2025-02-20 18:13:15 -0300

    update project with hhh files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,9 +10,6 @@ export default {
   theme: {
     extend: {
       colors: {
-        border: 'var(--border)',
-        input: 'var(--input)',
-        ring: 'var(--ring)',
         background: 'var(--background)',
         foreground: 'var(--foreground)',
         highlight: 'var(--highlight)',
@@ -33,22 +30,6 @@ export default {
         secondary: {
           DEFAULT: 'var(--secondary)',
           foreground: 'var(--secondary-foreground)',
-        },
-        destructive: {
-          DEFAULT: 'var(--destructive)',
-          foreground: 'var(--destructive-foreground)',
-        },
-        muted: {
-          DEFAULT: 'var(--muted)',
-          foreground: 'var(--muted-foreground)',
-        },
-        accent: {
-          DEFAULT: 'var(--accent)',
-          foreground: 'var(--accent-foreground)',
-        },
-        popover: {
-          DEFAULT: 'var(--popover)',
-          foreground: 'var(--popover-foreground)',
         },
         card: {
           DEFAULT: 'var(--card)',
