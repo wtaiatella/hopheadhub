d7849af3dbcfc68e8855d84d58f11c5a40ec6a72
Wagner Taiatella <wtaiatella@gmail.com>
2025-02-22 11:14:54 -0300

    Add search components

--- src/app/(main)/events/searchSection.tsx
+++ src/app/(main)/events/searchSection.tsx
@@ -1,23 +1,95 @@
-import { Button, Input } from 'antd';
-import { ArrowDownWideNarrow, SlidersHorizontal } from 'lucide-react';
+'use client'
+import { AutoComplete, Button, DatePicker, Dropdown, Input, Space } from 'antd'
+import type { MenuProps } from 'antd'
+import type { DatePickerProps } from 'antd'
+import { ArrowDownWideNarrow, ChevronDown, MapPin, SlidersHorizontal } from 'lucide-react'
+import { useState } from 'react'
+import '@/app/globals.css'
 
 export default function SearchSection() {
-  return (
-    <section className="container">
-      <div className="flex mt-10 px-4 gap-12 max-w-4xl mx-auto text-2xl">
-        <div className="flex items-center gap-2">
-          <SlidersHorizontal className="inline" size={24} />
-          <span>Filter</span>
-        </div>
-        <Input type="email" placeholder="Search events" />
-        <div className="flex items-center gap-2">
-          <span>Order</span>
-          <ArrowDownWideNarrow className="inline" size={24} />
-        </div>
-        <Button type="primary" className="flex items-center gap-2">
-          filter <ArrowDownWideNarrow className="inline" size={24} />
-        </Button>
-      </div>
-    </section>
-  );
+   const [value, setValue] = useState('')
+   const onChange = (data: string) => {
+      setValue(data)
+   }
+
+   const dropdownDateItems: MenuProps['items'] = [
+      {
+         label: (
+            <a href="https://www.antgroup.com" target="_blank" rel="noopener noreferrer">
+               1st menu item
+            </a>
+         ),
+         key: '0',
+      },
+      {
+         label: (
+            <a href="https://www.aliyun.com" target="_blank" rel="noopener noreferrer">
+               2nd menu item
+            </a>
+         ),
+         key: '1',
+      },
+      {
+         type: 'divider',
+      },
+      {
+         label: '3rd menu item',
+         key: '3',
+      },
+   ]
+
+   const onChangeDatePicker: DatePickerProps['onChange'] = (date, dateString) => {
+      console.log(date, dateString)
+   }
+
+   return (
+      <section className="container">
+         <h1 className="text-3xl font-semibold text-primary mt-10">Explore events</h1>
+         <div className="flex w-full items-center justify-between mt-4 gap-8">
+            <AutoComplete
+               value={value}
+               options={[]}
+               style={{ width: '100%' }}
+               onSelect={onChange}
+               onSearch={onChange}
+               onChange={onChange}
+               placeholder="Search events, partys, classes and more..."
+               allowClear
+            />
+            <div className="flex items-center border-b-2 border-primary">
+               <AutoComplete
+                  value={value}
+                  options={[]}
+                  style={{ minWidth: 200 }}
+                  onSelect={onChange}
+                  onSearch={onChange}
+                  onChange={onChange}
+                  placeholder="Search City"
+                  variant="borderless"
+               />
+               <MapPin className="w-5 h-5" />
+            </div>
+            <div className="flex items-center border-b-2 border-primary w-min-48">
+               <Dropdown
+                  className="cursor-pointer min-w-48"
+                  menu={{ items: dropdownDateItems }}
+                  trigger={['click']}
+               >
+                  <p className="w-full flex items-center justify-between">
+                     This weekend
+                     <ChevronDown />
+                  </p>
+               </Dropdown>
+               <DatePicker
+                  className="custom-datepicker cursor-pointer"
+                  allowClear={false}
+                  inputReadOnly={true}
+                  placement="bottomRight"
+                  onChange={onChangeDatePicker}
+                  variant="borderless"
+               />
+            </div>
+         </div>
+      </section>
+   )
 }
