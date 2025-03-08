9f49fe792efff635ac88b8c7e7779ca1c8fbf040
Wagner Taiatella <wtaiatella@gmail.com>
2025-03-07 23:23:21 -0300

    style: save with prettier format

--- src/app/clientProvider.tsx
+++ src/app/clientProvider.tsx
@@ -1,19 +1,18 @@
-'use client';
+'use client'
 
-import { ConfigProvider } from 'antd';
-import { AntdRegistry } from '@ant-design/nextjs-registry';
-import '@ant-design/v5-patch-for-react-19';
-import useAppStore from '@/stores/appStore';
-import "./globals.css";
+import { ConfigProvider } from 'antd'
+import { AntdRegistry } from '@ant-design/nextjs-registry'
+import '@ant-design/v5-patch-for-react-19'
+import useAppStore from '@/stores/appStore'
+import './globals.css'
+import React from 'react'
 
 export default function ClientProvider({ children }: { readonly children: React.ReactNode }) {
+   const theme = useAppStore(state => state.theme)
 
-    const theme = useAppStore((state) => state.theme);
-
-
-    return (
-        <ConfigProvider theme={theme}>
-            <AntdRegistry>{children}</AntdRegistry>
-        </ConfigProvider>
-    );
+   return (
+      <ConfigProvider theme={theme}>
+         <AntdRegistry>{children}</AntdRegistry>
+      </ConfigProvider>
+   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,78 +1,78 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  darkMode: ['class'],
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-        highlight: 'var(--highlight)',
-        header: {
-          text: 'var(--header-text)',
-          menu: 'var(--header-menu)',
-          background: 'var(--header-background)',
-          hover: 'var(--header-hover)',
-        },
-        gradient: {
-          from: 'var(--gradient-from)',
-          to: 'var(--gradient-from)',
-        },
-        primary: {
-          DEFAULT: 'var(--primary)',
-          foreground: 'var(--primary-foreground)',
-        },
-        secondary: {
-          DEFAULT: 'var(--secondary)',
-          foreground: 'var(--secondary-foreground)',
-        },
-        card: {
-          DEFAULT: 'var(--card)',
-          foreground: 'var(--card-foreground)',
-        },
+   darkMode: ['class'],
+   content: [
+      './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+   ],
+   theme: {
+      extend: {
+         colors: {
+            background: 'var(--background)',
+            foreground: 'var(--foreground)',
+            highlight: 'var(--highlight)',
+            header: {
+               text: 'var(--header-text)',
+               menu: 'var(--header-menu)',
+               background: 'var(--header-background)',
+               hover: 'var(--header-hover)',
+            },
+            gradient: {
+               from: 'var(--gradient-from)',
+               to: 'var(--gradient-to)',
+            },
+            primary: {
+               DEFAULT: 'var(--primary)',
+               foreground: 'var(--primary-foreground)',
+            },
+            secondary: {
+               DEFAULT: 'var(--secondary)',
+               foreground: 'var(--secondary-foreground)',
+            },
+            card: {
+               DEFAULT: 'var(--card)',
+               foreground: 'var(--card-foreground)',
+            },
+         },
+         fontFamily: {
+            sans: ['var(--font-sans)'],
+            serif: ['var(--font-serif)'],
+            mono: ['var(--font-mono)'],
+         },
+         borderRadius: {
+            lg: 'var(--radius)',
+            md: 'calc(var(--radius) - 2px)',
+            sm: 'calc(var(--radius) - 4px)',
+         },
+         keyframes: {
+            'accordion-down': {
+               from: { height: '0' },
+               to: { height: 'var(--radix-accordion-content-height)' },
+            },
+            'accordion-up': {
+               from: { height: 'var(--radix-accordion-content-height)' },
+               to: { height: '0' },
+            },
+         },
+         animation: {
+            'accordion-down': 'accordion-down 0.2s ease-out',
+            'accordion-up': 'accordion-up 0.2s ease-out',
+         },
+         screens: {
+            '2sm': '450px',
+            laptop: '1400px',
+         },
+         spacing: {
+            '76': '304px',
+            '114': '456px',
+            '120': '480px',
+         },
+         gridTemplateColumns: {
+            auth: '1.5fr 2fr',
+         },
       },
-      fontFamily: {
-        sans: ['var(--font-sans)'],
-        serif: ['var(--font-serif)'],
-        mono: ['var(--font-mono)'],
-      },
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
-      },
-      keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
-        },
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-      },
-      screens: {
-        '2sm': '450px',
-        laptop: '1400px',
-      },
-      spacing: {
-        '76': '304px',
-        '114': '456px',
-        '120': '480px',
-      },
-      gridTemplateColumns: {
-        auth: '1.5fr 2fr',
-      },
-    },
-  },
-  plugins: [require('tailwindcss-animate')],
-} satisfies Config;
+   },
+   plugins: [require('tailwindcss-animate')],
+} satisfies Config
